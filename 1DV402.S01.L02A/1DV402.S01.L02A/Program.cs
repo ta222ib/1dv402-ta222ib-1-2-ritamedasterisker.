ff8328397757d289eeb6c675b30f9892bbc57b01
ta222ib <ta222ib@student.lnu.se>
2014-10-12 21:45:49 +0200

    finish S01L02A

---
+++ 1DV402.S01.L02A/1DV402.S01.L02A/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S01.L02A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                int b = i % 3;
+                switch (b)
+                {
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                }
+                if (i % 2 > 0)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < 39; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            Console.ResetColor();  
+        }
+    }
+}
